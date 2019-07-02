9daac4a523cb03931562d1a5cd99282f665c65c7
Dmitry <dmitrysemm@gmail.com>
2019-07-02 18:06:01 +0300

    Implemented task 2-2

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,27 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.lastChunk = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const chunkStr = chunk.toString();
+    const splittedChunks = chunkStr.split(os.EOL);
+
+    this.lastChunk += splittedChunks.shift();
+
+    while (splittedChunks.length) {
+      this.push(this.lastChunk);
+      this.lastChunk = splittedChunks.shift();
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.lastChunk);
+    callback();
   }
 }
 
